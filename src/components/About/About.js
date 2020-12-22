23af2d9a50fdbe87e29fbc4500ae3064a9b69626
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-12-22 13:55:24 +0100

    Add title and text to about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,12 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <section className="about">
+      <h2 className="about__title">What's the proposal of this app?</h2>
+      <p className="about__text">
+        Se trata de un calendario en el que cada día vas apuntado qué tal ha ido
+        el día eligiendo un smiley. La idea es poder ver un periodo de tiempo en
+        contexto para que los días malos no puedan a los buenos.
+      </p>
       <Link to="/" className="link">
         Back
       </Link>
