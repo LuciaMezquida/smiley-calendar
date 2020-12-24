13061dbc3dfdd38a72d2c988f98eaf1ab13fbcf1
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-12-24 18:50:23 +0100

    Add switch buttons to DaysList

--- src/components/DaysList/DaysList.js
+++ src/components/DaysList/DaysList.js
@@ -3,6 +3,10 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import SmileyCard from "../SmileyCard/SmileyCard";
+import Switch from "@material-ui/core/Switch";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
 
 class DaysList extends Component {
   render() {
@@ -16,6 +20,17 @@ class DaysList extends Component {
     return (
       <main className="mainList">
         <div className="mainList__button-container">
+          <FormControl component="fieldset">
+            <FormGroup aria-label="position" row>
+              <FormControlLabel
+                value="start"
+                control={<Switch color="primary" />}
+                label=":)"
+                labelPlacement="start"
+                className="mainList__button-container--smiley"
+              />
+            </FormGroup>
+          </FormControl>
           <Link
             className="mainList__button-container--button link"
             to="/edition"
@@ -23,6 +38,17 @@ class DaysList extends Component {
           >
             +
           </Link>
+          <FormControl component="fieldset">
+            <FormGroup aria-label="position" row>
+              <FormControlLabel
+                value="end"
+                control={<Switch color="primary" />}
+                label=":("
+                labelPlacement="end"
+                className="mainList__button-container--smiley"
+              />
+            </FormGroup>
+          </FormControl>
         </div>
         <ul className="mainList__year">{smileyList}</ul>
       </main>
